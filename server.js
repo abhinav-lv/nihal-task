5a3663fb02f8b866072058191a4ba8cc76e4f319
Abhinav LV <abhinavlv@yahoo.com>
2024-12-06 23:33:10 +0530

    added scripts

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ import { cwd } from "process";
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(cwd(), "client", "dist")));
 app.use(express.urlencoded({ extended: true }));
 
 const upload = multer({ dest: "videos/" });
@@ -42,6 +43,10 @@ app.get("/video/:id", (req, res) => {
   } else {
     res.status(404).send("Video not found");
   }
+});
+
+app.get("*", (req, res) => {
+  res.sendFile("/index.html");
 });
 
 // Start server
